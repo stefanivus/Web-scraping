1e25e47ae8e73d216d2f29f301e3fa28f641d7ac
stefanivus <stefanivus@gmail.com>
2017-04-18 10:09:44 +0200

    Add files via upload
    The user inputs a youtube channel name, and the program outputs how many views their most popular video has

---
+++ Most viewed video.py
@@ -0,0 +1,17 @@
+from urllib import request
+import bs4 as bs
+
+while(True):
+    user = input("Youtube channel name: ")
+    url = "https://www.youtube.com/user/"+ user +"/videos?sort=p&view=0&flow=grid"
+    html = request.urlopen(url).read()
+    soup = bs.BeautifulSoup(html,'lxml')
+    for p in soup.find_all('ul','yt-lockup-meta-info'):
+        views = p.text
+        break
+    i = 1
+    s = "k"
+    while s[len(s)-1] != " ":
+        s = views[0:i]
+        i += 1
+    print(s)
